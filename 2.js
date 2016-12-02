c5130de0eb17f33189e7d33e98bc5694ba9f8c49
THOM Asbjorn <asbjorn.thom@ext.soprasteria.com>
2016-12-02 10:04:48 +0100

    2

---
+++ 2.js
@@ -0,0 +1,19 @@
+function fib(){
+	num = 1;
+	num2 = 0;
+	sum = 0;
+	fib = 0;
+	while (sum < 4000000000){
+		fib = num + num2;
+		num2 = num;
+		num = fib;
+		if((fib % 2 == 0)){ 
+			sum += fib;
+		}
+		if(sum < 4000000000){console.log(sum);}
+	}
+}
+
+console.time('fib');
+fib();
+console.timeEnd('fib');
